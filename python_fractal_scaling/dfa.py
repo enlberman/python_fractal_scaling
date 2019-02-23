4453ee8ff0e29d992fc4c50ec2a6b464ff894101
andrew-stier <andrewstier@uchicago.edu>
2019-02-23 08:53:56 -0600

    add option for random walk data

--- python_fractal_scaling/dfa.py
+++ python_fractal_scaling/dfa.py
@@ -2,16 +2,18 @@ import numpy
 from sklearn.linear_model import LinearRegression
 
 
-def dfa(x: numpy.ndarray, max_window_size: int,  min_widow_size: int =3):
+def dfa(x: numpy.ndarray, max_window_size: int, min_widow_size: int = 3, random_walk: bool = False):
     """
     Expects X to be a time series of T rows and n columns.
+    :param random_walk: Is the data x like a random walk?
     :param x:
     :param max_window_size:
     :param min_widow_size:
     :return: a list whose first element is the vector of DFA exponents for the n features and whose second element is the r_squared for the log-log regression
     """
+
     def unbounded(xx):
-        return numpy.cumsum(xx - xx.mean(0), axis=0)
+        return numpy.cumsum(xx - xx.mean(0), axis=0) if not random_walk else xx
 
     def windows(yt, n):
         return numpy.split(yt, numpy.arange(n, yt.shape[0], step=n))
@@ -29,7 +31,7 @@ def dfa(x: numpy.ndarray, max_window_size: int,  min_widow_size: int =3):
 
     features = numpy.log(numpy.arange(min_widow_size, max_window_size))
     y = numpy.log(f(x, mn=min_widow_size, mx=max_window_size))
-    lr = LinearRegression().fit(features,y)
+    lr = LinearRegression().fit(features, y)
     r_squared = lr.score(features, y)
-    hurst = lr.coef_.flatten()
+    hurst = lr.coef_.flatten() + (1 if random_walk else 0)
     return hurst, r_squared
